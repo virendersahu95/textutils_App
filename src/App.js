6ac73a24f0b055ce558dedc1f28bdf08f8d134be
virendersahu95 <virender.sahu@globallogic.com>
2022-05-01 20:21:33 +0530

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Navbar from './components/Navbar';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return <Navbar title="TextUtils" aboutText="About TextUtils" />;
 }
